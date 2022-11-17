127456a22a435af7150fad578449e3e3cde93be0
ziang jia <zjia@ksmconsulting.com>
2022-11-17 18:57:36 -0500

    allow pre-fix centroid

--- databricks/build.sh
+++ databricks/build.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+## Build the customize Spark mllib
 ./build/mvn -DskipTests -Phive-2.3 -Phive-thriftserver -Dhadoop-3.2 -pl :spark-mllib_2.12 clean package
+## Install databricks CLI
+pip install databricks-cli
+## Authentication with Databricks personal access token. You need both Databricks host and Token
+databricks configure --token
+# Databricks Host (should begin with https://):
+# Token: 
+## Copy all necessary resources to Databricks DBFS
 databricks fs cp mllib/target/spark-mllib_2.12-3.2.1.jar dbfs:/customize-resources/jars/----workspace_spark_3_2--mllib--mllib-hive-2.3__hadoop-3.2_2.12_deploy_shaded.jar
+databricks fs cp databricks/databricks/mllib-install.sh dbfs:/customize-resources/init-scripts/mllib-install.sh
--- databricks/mllib-install.sh
+++ databricks/mllib-install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+## Copy this bash file to Databricks DBFS and configure it as an initscript
 rm -rf /databricks/jars/----workspace_spark_3_2--mllib--mllib-hive-2.3__hadoop-3.2_2.12_deploy_shaded.jar
 cp /dbfs/databricks/jars/----workspace_spark_3_2--mllib--mllib-hive-2.3__hadoop-3.2_2.12_deploy_shaded.jar /databricks/jars/
